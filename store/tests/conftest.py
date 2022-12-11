c596163ebe53c5f3efc253647b2e9e47b6adf727
Roxey <32243415+roxey804@users.noreply.github.com>
2022-12-11 23:50:10 +0000

    update tests

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,26 +1,10 @@
 import pytest
-import datetime
-from blog.models import Post, Author, Tags
+from store.models import Product
 
 @pytest.fixture
-def author_fixture():
-    return Author.objects.create(
-        name="Mock Author"
-    )
-
-@pytest.fixture
-def tag_fixture():
-    return Tags.objects.create(
-        name="diy"
-    )
-
-@pytest.fixture
-def blog_post_fixture():
-    return Post.objects.create(
-        slug='home-diy',
-        title='Mock title',
-        description='desc',
-        date=datetime.date.today(),
-        image='http://google.com',
-        image_alt='alt text'
+def product_fixture():
+    return Product.objects.create(
+        name="Mock product",
+        description="desc",
+        price=1,
     )
